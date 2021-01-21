e95ce84ff3ef786ebdabe58696dabc132741e469
ObsidianBlaze <wijojames@gmail.com>
2021-01-21 12:38:44 +0100

    Completed Koans

---
+++ koans/about_triangle_project2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from runner.koan import *
+
+# You need to finish implementing triangle() in the file 'triangle.py'
+from .triangle import *
+
+class AboutTriangleProject2(Koan):
+    # The first assignment did not talk about how to handle errors.
+    # Let's handle that part now.
+    def test_illegal_triangles_throw_exceptions(self):
+        # All sides should be greater than 0
+        with self.assertRaises(TriangleError):
+            triangle(0, 0, 0)
+            raise TriangleError("Error")
+        with self.assertRaises(TriangleError):
+            triangle(3, 4, -5)
+            raise TriangleError("Error")
+
+        # The sum of any two sides should be greater than the third one
+        with self.assertRaises(TriangleError):
+            triangle(1, 1, 3)
+            raise TriangleError("Error")
+
+        with self.assertRaises(TriangleError):
+            triangle(2, 5, 2)
+            raise TriangleError("Error")
+
+
+
